e8cea8ce2cfe3b61b9b2949a14ac07a34126aa9d
CCrisstian <crisstiann.c@gmail.com>
2024-02-07 11:58:31 -0300

    Tarea terminada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\n\tTarea: Expresión Lambda que elimine espacios\n");
+
+        Function<String, String> Eliminar_Espacios_Comas_Puntos = entrada ->
+                entrada.replace(" ", "").   /*Elimina espacios en blanco*/
+                        replace(",", "").   /*Elimina comas*/
+                        replace(".","").    /*Elimina puntos*/
+                        toUpperCase();                      /*Convierte las letras en mayúsculas*/
+
+
+        String palabra = "Hola, mundo. ¿Como Estas?";
+        System.out.println("Entrada: " + palabra);
+        System.out.println("Salida: " + Eliminar_Espacios_Comas_Puntos.apply(palabra));
     }
 }
